4542dd17a093e6669a23826a64e165f786cdc0be
Mark Tripney <marktripney@icloud.com>
2022-06-09 09:01:29 +0100

    Add 'trash' icon; combine 'summary' updates.

--- js/index.js
+++ js/index.js
@@ -22,18 +22,12 @@ const setAttributes = (element, attributes) => {
   }
 };
 
-const toggleNotesField = () => {
+const toggleSummaryField = () => {
   if (totalCost > 0) {
     notes.textContent = "We accept cash, credit card, or PayPal";
-  } else {
-    notes.textContent = "";
-  }
-};
-
-const toggleInvoiceBtn = () => {
-  if (totalCost > 0) {
     sendInvoiceBtn.disabled = false;
   } else {
+    notes.textContent = "";
     sendInvoiceBtn.disabled = true;
   }
 };
@@ -49,8 +43,7 @@ const removeService = (e, service) => {
   renderService();
   totalCost -= services[service];
   total.textContent = `$${totalCost}`;
-  toggleInvoiceBtn();
-  toggleNotesField();
+  toggleSummaryField();
 };
 
 const renderService = () => {
@@ -80,7 +73,22 @@ const renderService = () => {
       class: "service-cost",
     });
     serviceName.textContent = service;
-    removeBtn.textContent = "Remove";
+    removeBtn.innerHTML = `
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="remove-icon"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="orange"
+      stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+          />
+      </svg>
+    `;
     removeBtn.addEventListener("click", (e) => {
       removeService(e, service);
     });
@@ -93,8 +101,7 @@ const renderService = () => {
     serviceDiv.appendChild(serviceCostDiv);
     servicesContainer.appendChild(serviceDiv);
   });
-  toggleInvoiceBtn();
-  toggleNotesField();
+  toggleSummaryField();
 };
 
 // Loop over services object and create a button for each service
@@ -126,6 +133,5 @@ sendInvoiceBtn.addEventListener("click", () => {
   renderService();
   totalCost = 0;
   total.textContent = `$${totalCost}`;
-  toggleInvoiceBtn();
-  toggleNotesField();
+  toggleSummaryField();
 });
